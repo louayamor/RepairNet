18e2731ad2e6db2d128c5e34524ca59cbce79182
louayamor <amor.louay20@gmail.com>
2025-04-09 13:48:45 +0100

    Initialized Repairnet and Created MVC structure

---
+++ src/main/java/itbs/louayamor/repairnet/RepairNetApplication.java
@@ -0,0 +1,13 @@
+package itbs.louayamor.repairnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itbs/louayamor/repairnet/RepairNetApplicationTests.java
@@ -0,0 +1,13 @@
+package itbs.louayamor.repairnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
