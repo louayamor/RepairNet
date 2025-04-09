4fd3bd63f3ea1be11e03f77177b1dd9d04234b35
louayamor <amor.louay20@gmail.com>
2025-04-09 17:46:33 +0100

    Fault repository done

--- src/main/java/itbs/louayamor/repairnet/repository/FaultRepo.java
+++ src/main/java/itbs/louayamor/repairnet/repository/FaultRepo.java
@@ -1,5 +1,32 @@
 package itbs.louayamor.repairnet.repository;
 
-public interface FaultRepo {
+import itbs.louayamor.repairnet.bean.Fault;
+import itbs.louayamor.repairnet.bean.Equipment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface FaultRepo extends JpaRepository<Fault, Long> {
+
+    Fault save(Fault fault);
+
+    Optional<Fault> findById(Long id);
+
+    List<Fault> findAll();
+
+    void deleteById(Long id);
+
+    boolean existsById(Long id);
+
+    Fault findByUuid(UUID uuid);
+
+    List<Fault> findByEquipment(Equipment equipment);
+
+    List<Fault> findAllByOrderByDateReported();
+
+    Page<Fault> findAll(Pageable pageable);
 }
