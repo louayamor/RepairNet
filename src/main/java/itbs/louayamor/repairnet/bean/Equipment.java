4dad5114e05a2ae7cb01db541c9aa76b8246697e
louayamor <amor.louay20@gmail.com>
2025-04-09 14:11:45 +0100

    Finished Equipment bean referring to the specifications

--- src/main/java/itbs/louayamor/repairnet/bean/Equipment.java
+++ src/main/java/itbs/louayamor/repairnet/bean/Equipment.java
@@ -1,5 +1,32 @@
 package itbs.louayamor.repairnet.bean;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "equipment")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Equipment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID uuid = UUID.randomUUID();
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EquipmentStatus status;
+
+    private LocalDate acquisitionDate;
 }
