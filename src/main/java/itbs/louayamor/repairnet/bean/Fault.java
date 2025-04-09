7851347c89184d86e41310af88211e5103346cba
louayamor <amor.louay20@gmail.com>
2025-04-09 17:19:40 +0100

    Added Fault bean according to the specifications

--- src/main/java/itbs/louayamor/repairnet/bean/Fault.java
+++ src/main/java/itbs/louayamor/repairnet/bean/Fault.java
@@ -1,5 +1,37 @@
 package itbs.louayamor.repairnet.bean;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "faults")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Fault {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID uuid = UUID.randomUUID();  
+
+    @Column(nullable = false)
+    private String description;  
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FaultCategory category;  
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "equipment_id", nullable = false)
+    private Equipment equipment;  
+
+    @Column(name = "report_date", nullable = false)
+    private LocalDateTime reportDate;  
 }
---
+++ src/main/java/itbs/louayamor/repairnet/bean/FaultCategory.java
@@ -0,0 +1,12 @@
+package itbs.louayamor.repairnet.bean;
+
+public enum FaultCategory {
+	HARDWARE,
+    SOFTWARE,
+    NETWORK,
+    PERIPHERAL,
+    OVERHEATING,
+    BATTERY_CHARGING,
+    PHYSICAL_DAMAGE,
+    MISCELLANEOUS
+}
