78f1fe4e72fdc5e690d0011515ed110b0f9ba540
louayamor <amor.louay20@gmail.com>
2025-04-09 17:39:43 +0100

    Created repo for equipment

--- src/main/java/itbs/louayamor/repairnet/repository/EquipmentRepo.java
+++ src/main/java/itbs/louayamor/repairnet/repository/EquipmentRepo.java
@@ -1,5 +1,41 @@
 package itbs.louayamor.repairnet.repository;
 
-public interface EquipmentRepo {
+import itbs.louayamor.repairnet.bean.Equipment;
+import itbs.louayamor.repairnet.bean.EquipmentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+public interface EquipmentRepo extends JpaRepository<Equipment, Long> {
+
+    Equipment save(Equipment equipment);
+
+    Optional<Equipment> findById(Long id);
+
+    //Main CRUD
+    
+    List<Equipment> findAll();
+
+    void deleteById(Long id);
+
+    boolean existsById(Long id);
+
+    //Custom Methods
+
+    Equipment findByUuid(UUID uuid);
+
+    List<Equipment> findByStatus(EquipmentStatus status);
+
+    List<Equipment> findByNameContainingIgnoreCase(String name);
+
+    List<Equipment> findByStatusOrderByAcquisitionDateAsc(EquipmentStatus status);
+
+    Page<Equipment> findAll(Pageable pageable);
+
+    List<Equipment> findByNameContainingIgnoreCaseOrStatus(String name, EquipmentStatus status);
+
+    void deleteByStatus(EquipmentStatus status);
 }
