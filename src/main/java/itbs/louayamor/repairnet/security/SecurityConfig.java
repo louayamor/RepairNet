77db67ce163f63533dfd953edaf5c4c5309c46bc
louayamor <amor.louay20@gmail.com>
2025-04-10 15:41:51 +0100

    Added custom authentication entry point to stop redirect loops

--- src/main/java/itbs/louayamor/repairnet/security/SecurityConfig.java
+++ src/main/java/itbs/louayamor/repairnet/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package itbs.louayamor.repairnet.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,25 +9,37 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import itbs.louayamor.repairnet.bean.CustomAuthenticationEntryPoint;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public SecurityConfig(CustomAuthenticationEntryPoint authenticationEntryPoint) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/equipment/**","/api/fault/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .loginPage("/login").permitAll()
-            )
-            .logout(logout -> logout.permitAll());
-
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/equipment/**", "/api/faults/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                                .authenticationEntryPoint(authenticationEntryPoint)
+                )
+                .formLogin(form -> form.disable()) 
+                .httpBasic(withDefaults()); 
         return http.build();
     }
 
@@ -35,7 +48,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // Example in-memory user for login testing (optional)
     @Bean
     public UserDetailsService userDetailsService() {
         var user = User.withUsername("user")
